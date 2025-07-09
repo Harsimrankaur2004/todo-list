2e5b9027877a62e810d986daf7e26cc38b3f3ec2
Harsimrankaur2004 <sunshinelife067@gmail.com>
2025-07-09 17:51:05 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+let todoList = JSON.parse(localStorage.getItem('todoList')) || [
+  {
+    name: "Buy groceries",
+    done: false
+  },
+  {
+    name: "Read book",
+    done: false
+  }
+];
+
+saveToStorage();
+
+let inputTask = document.getElementById("inputTask");
+
+// It saves everything in localStorage
+function saveToStorage() {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+// Add an event listener to add-button
+document.querySelector(".add-btn")
+  .addEventListener("click", () => {
+    let name = inputTask.value.trim();
+
+    if(name === "") return;
+
+    todoList.push({ name, done: false });
+    saveToStorage();
+    renderPage();
+    inputTask.value = '';
+  });
+
+// Add an event listener to inputTask
+inputTask.addEventListener("keydown", event => {
+  if (event.key === "Enter") {
+    let name = inputTask.value.trim();
+
+    if(name === "") return;
+
+    todoList.push({ name, done: false });
+    saveToStorage();
+    renderPage();
+    inputTask.value = '';
+  }
+});
+
+// It will render the whole page
+function renderPage() {
+
+  // A variable that saves the html
+  let todoHTML = "";
+
+  // It generates the HTML
+  todoList.forEach((todo, index) => {
+    let { name } = todo;
+    todoHTML += `
+    <div class="todo-task-box id=${index}">
+      <div id="${index}" class="check-box ${todo.done ? "checked": ""}">
+       
+        <!-- <img src="image/tick.svg"> -->
+      </div>
+      <div id=${index} class="todo-task ${todo.done ? "done" : ""}">
+        ${name}
+      </div>
+      <button id=${index} class="dlt-btn">
+        <img src="image/delete.svg" alt="">
+      </button>
+    </div>
+  `;
+  });
+
+  document.querySelector(".todo-task-container")
+    .innerHTML = todoHTML;
+
+  // console.log(todoHTML);
+
+
+  // This code checked and unchecked the tick-box
+  document.querySelectorAll(".check-box")
+    .forEach((checkBox, index) => {
+      checkBox.addEventListener("click", () => {
+        checkBox.classList.toggle("checked");
+        const taskText = checkBox.nextElementSibling;
+
+        taskText.classList.toggle("done");
+
+        todoList[index].done = checkBox.classList.contains("checked");
+        saveToStorage();
+      });
+    });
+
+  // Add an event listener to delete button
+  document.querySelectorAll(".dlt-btn")
+    .forEach((dlt, index) => {
+      dlt.addEventListener("click", () => {
+        todoList.splice(index, 1);
+        saveToStorage();
+        renderPage();
+        
+      })
+    })
+}
+
+renderPage();
